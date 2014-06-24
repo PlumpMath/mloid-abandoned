6b8cd2cd1f2ae693fdb0aae7ab608af7be24819d
Michael Arntzenius <daekharel@gmail.com>
2014-06-24 17:01:48 -0400

    initial import

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject mloid "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  ;; TODO: url
+  ;; :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main mloid.core)
---
+++ src/mloid/core.clj
@@ -0,0 +1,6 @@
+(ns mloid.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "this is silly"))
